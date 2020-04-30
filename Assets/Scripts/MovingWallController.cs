19f3672706c7c9e3b2fc6604dd418d6a81c0ad74
yuto <yuhto.1027@gmail.com>
2020-04-30 18:34:23 +0900

    完成

---
+++ Assets/Scripts/MovingWallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWallController : MonoBehaviour
+{
+    private Vector3 targetpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(targetpos.x, Mathf.Sin(Time.time) + targetpos.y + 1, targetpos.z);
+    }
+}
